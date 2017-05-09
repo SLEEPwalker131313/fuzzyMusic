64f08fd0fc7e4fa70d01d171ac68068e98487844
SLEEPwalker131313 <petrunin-vn@yandex.ru>
2017-05-09 15:48:26 +0300

    Yandex music
    Frame was added

---
+++ js/action.js
@@ -0,0 +1,4 @@
+function applyChange(){
+  console.log(1);
+  $('#songList').append('<li><iframe frameborder="0" style="border:none;width:600px;height:100px;" width="600" height="100" src="https://music.yandex.ru/iframe/#track/17893825/1978897/">Слушайте <a href="https://music.yandex.ru/track/17893825">Они такая мелочь</a> — <a href="https://music.yandex.ru/artist/1546363">Фиксики</a> на Яндекс.Музыке</iframe>')
+}
